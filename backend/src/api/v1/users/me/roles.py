0d07c689ca1595a213aed3d56eb857874015584c
Mark <wixw1cky7@gmail.com>
2026-02-02 16:09:47 +0700

    Fix some import issues

--- backend/src/api/v1/users/me/roles.py
+++ backend/src/api/v1/users/me/roles.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from database.relational_db import User
-from domain.users import RoleModel
+from domain.roles import RoleModel
 from core.config import Settings
 from core.security import auth_user
 
